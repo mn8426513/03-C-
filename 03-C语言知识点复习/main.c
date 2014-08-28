e29c92e8918b684da8af5c4249ea4249f6a5ec55
Mac <Mac@MacdeMac-Pro.local>
2014-08-28 08:50:18 +0800

    Initial Commit

---
+++ 03-C语言知识点复习/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  03-C语言知识点复习
+//
+//  Created by Mac on 14-8-28.
+//  Copyright (c) 2014年 MN&WC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
